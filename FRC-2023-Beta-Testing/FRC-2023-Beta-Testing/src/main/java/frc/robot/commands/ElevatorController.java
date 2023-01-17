bd896ab2c31dc4a9c70d8310a11f0d6323949842
Kushagra S <93955597+Ghost-python07@users.noreply.github.com>
2023-01-17 11:23:34 -0500

    Update ElevatorController.java
    Change the one line in execute()

--- FRC-2023-Beta-Testing/FRC-2023-Beta-Testing/src/main/java/frc/robot/commands/ElevatorController.java
+++ FRC-2023-Beta-Testing/FRC-2023-Beta-Testing/src/main/java/frc/robot/commands/ElevatorController.java
@@ -32,7 +32,7 @@ public class ElevatorController extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_elevatorSubsystem.set(e_controller.getLeftY());
+    m_elevatorSubsystem.setPercentOutput(e_controller.getLeftY());
   }
 
   // Called once the command ends or is interrupted.
