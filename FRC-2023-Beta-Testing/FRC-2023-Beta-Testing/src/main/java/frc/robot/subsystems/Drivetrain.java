d0e6c7005f25257375d54116d4b2635685adfcef
EtherexStudios <102874323+EtherexStudios@users.noreply.github.com>
2023-01-19 15:47:10 -0500

    Constants in drivetrain

--- FRC-2023-Beta-Testing/FRC-2023-Beta-Testing/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ FRC-2023-Beta-Testing/FRC-2023-Beta-Testing/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -16,41 +16,39 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.Constants;
 
 /** Represents a differential drive style drivetrain. */
 public class Drivetrain extends SubsystemBase {
-  public static final double kMaxSpeed = 3.0; // meters per second
-  public static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation per second
 
-  private static final double kTrackWidth = 0.381 * 2; // meters
-  private static final double kWheelRadius = 0.0508; // meters
-  private static final int kEncoderResolution = 4096;
+  //reasign to all values to the correct ports in the constants file
+  private final MotorController m_leftLeader = new WPI_TalonSRX(Constants.DriveConstants.kLeftMotor1Port);
+  private final MotorController m_leftFollower = new WPI_TalonSRX(Constants.DriveConstants.kLeftMotor2Port);
+  private final MotorController m_rightLeader = new WPI_TalonSRX(Constants.DriveConstants.kRightMotor1Port);
+  private final MotorController m_rightFollower = new WPI_TalonSRX(Constants.DriveConstants.kRightMotor2Port);
 
-  private final MotorController m_leftLeader = new WPI_TalonSRX(1);
-  private final MotorController m_leftFollower = new WPI_TalonSRX(2); 
-  private final MotorController m_rightLeader = new WPI_TalonSRX(3);
-  private final MotorController m_rightFollower = new WPI_TalonSRX(4);
-
-  private final Encoder m_leftEncoder = new Encoder(0, 1);
-  private final Encoder m_rightEncoder = new Encoder(2, 3);
+  //reasign to all values to the correct ports in the constants file
+  private final Encoder m_leftEncoder = new Encoder(Constants.DriveConstants.kLeftEncoderPorts[0], Constants.DriveConstants.kLeftEncoderPorts[1]);
+  private final Encoder m_rightEncoder = new Encoder(Constants.DriveConstants.kRightEncoderPorts[0], Constants.DriveConstants.kRightEncoderPorts[1]);
 
   private final MotorControllerGroup m_leftGroup =
       new MotorControllerGroup(m_leftLeader, m_leftFollower);
   private final MotorControllerGroup m_rightGroup =
       new MotorControllerGroup(m_rightLeader, m_rightFollower);
 
-  private final AnalogGyro m_gyro = new AnalogGyro(0);
+  
+  private final AnalogGyro m_gyro = new AnalogGyro(Constants.DriveConstants.kGyroPort);
 
-  private final PIDController m_leftPIDController = new PIDController(1, 0, 0);
-  private final PIDController m_rightPIDController = new PIDController(1, 0, 0);
+  private final PIDController m_leftPIDController = new PIDController(Constants.DriveConstants.kP, Constants.DriveConstants.kI, Constants.DriveConstants.kD);
+  private final PIDController m_rightPIDController = new PIDController(Constants.DriveConstants.kP, Constants.DriveConstants.kI, Constants.DriveConstants.kD);
 
   private final DifferentialDriveKinematics m_kinematics =
-      new DifferentialDriveKinematics(kTrackWidth);
+      new DifferentialDriveKinematics(Constants.DriveConstants.kTrackWidth);
 
   private final DifferentialDriveOdometry m_odometry;
 
   // Gains are for example purposes only - must be determined for your own robot!
-  private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(1, 3);
+  private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(Constants.DriveConstants.kS, Constants.DriveConstants.kV, Constants.DriveConstants.kA);
 
   /**
    * Constructs a differential drive object. Sets the encoder distance per pulse and resets the
@@ -62,13 +60,13 @@ public class Drivetrain extends SubsystemBase {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightGroup.setInverted(true); // changed back to true
+    m_rightGroup.setInverted(true);
 
     // Set the distance per pulse for the drive encoders. We can simply use the
     // distance traveled for one rotation of the wheel divided by the encoder
     // resolution.
-    m_leftEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
-    m_rightEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
+    m_leftEncoder.setDistancePerPulse(Constants.DriveConstants.kEncoderDistancePerPulse);
+    m_rightEncoder.setDistancePerPulse(Constants.DriveConstants.kEncoderDistancePerPulse);
 
     m_leftEncoder.reset();
     m_rightEncoder.reset();
