ef4cc27b3f19becd9c5a2558a7dcd73daec19c77
EtherexStudios <102874323+EtherexStudios@users.noreply.github.com>
2023-01-28 13:15:07 -0500

    Update ArmPIDandFeedForward.java

--- src/main/java/frc/robot/subsystems/ArmPIDandFeedForward.java
+++ src/main/java/frc/robot/subsystems/ArmPIDandFeedForward.java
@@ -1,81 +1,88 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-// import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-// import com.ctre.phoenix.motorcontrol.NeutralMode;
-// import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-// import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import edu.wpi.first.math.controller;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
-// public class ArmPIDandFeedForward extends SubsystemBase {
-//     private final WPI_TalonFX talon;
-//     private static Arm instance = null;
-//     private static double lastPercentSpeed; 
+public class ArmPIDandFeedForward extends SubsystemBase {
+    private final WPI_TalonFX talon;
+    private static Arm instance = null;
+    private static double lastPercentSpeed; 
   
   
-//     private static final double kS;
-//     private static final double kG;
-//     private static final double kV;
-//     private static final double kA;
-//     private static final ArmFeedforward feedforward;
+    private static final double kS = 0.0;
+    private static final double kG = 0.0;
+    private static final double kV = 0.0;
+    private static final double kA = 0.0;
     
     
+    private static ArmFeedforward m_feedforward;
 
-//     public ArmPIDandFeedForward() {
-//         this.talon = new WPI_TalonFX(5); // one instance of TalonSRX, replaced IntakeConstants.TALON_ID
-//         lastPercentSpeed = 0;
-//         TalonFXConfiguration falconConfig = new TalonFXConfiguration();
+    private static final double kStartAngle = -23.0;
 
-//         falconConfig.slot0.kP = 0;
-//         falconConfig.slot0.kI = 0;
-//         falconConfig.slot0.kD = 0;
+    private static final double kEncoderResolution = 4096;
 
-//         falconConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
-//         falconConfig.voltageCompSaturation = 12;
+    private static final double kVelocityMultiplier = 1.0;
 
-//         falconConfig.nominalOutputForward = 0;
-//         falconConfig.nominalOutputReverse = 0;
-//         falconConfig.peakOutputForward = 1;
-//         falconConfig.peakOutputReverse = -1;
+    private static double kAngle = kStartAngle;
+    private static  double kAngleIncrement;
+    private static double kPreviosPosition;
 
-//         talon.configAllSettings(falconConfig);
-//         talon.setNeutralMode(NeutralMode.Coast);
+    public ArmPIDandFeedForward() {
+        this.talon = new WPI_TalonFX(5); // one instance of TalonSRX, replaced IntakeConstants.TALON_ID
+        lastPercentSpeed = 0;
+        TalonFXConfiguration falconConfig = new TalonFXConfiguration();
+
+        falconConfig.slot0.kP = 0;
+        falconConfig.slot0.kI = 0;
+        falconConfig.slot0.kD = 0;
+
+        falconConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
+        falconConfig.voltageCompSaturation = 12;
+
+        falconConfig.nominalOutputForward = 0;
+        falconConfig.nominalOutputReverse = 0;
+        falconConfig.peakOutputForward = 1;
+        falconConfig.peakOutputReverse = -1;
+
+        talon.configAllSettings(falconConfig);
+        talon.setNeutralMode(NeutralMode.Coast);
         
-//         // kS and kG should have units of volts, kV should have units of volts * seconds / radians, and kA should have units of volts * seconds^2 / radians.
-//         // Units must be consistent! TODO: get feedfowrward gains tuned
-//         kS = 0.0;
-//         kG = 0.0;
-//         kV = 0.0;
-//         kA = 0.0;
+        // kS and kG should have units of volts, kV should have units of volts * seconds / radians, and kA should have units of volts * seconds^2 / radians.
+        // Units must be consistent! TODO: get feedfowrward gains tuned
       
-//         feedforward = new ArmFeedforward(kS, kG, kV, kA);
+        m_feedforward = new ArmFeedforward(kS, kG, kV, kA);
         
-//     }
-
-//     // Singleton class, call getInstance to access instead of the constructor.
-//     public static Arm getInstance() {
-//         if (instance == null) {
-//             instance = new Arm();
-//         }
-//         return instance;
-//     }
-
-//     public void setPercentOutput(double percent) {
+        kAngleIncrement = 360 / kEncoderResolution;
         
-//         if(Math.abs(percent) < 0.08) {
-//             percent = 0;
-//             //talon.enableBrakeMode(true); 
-//         }
-      
-//         if(percent == 0) {
-//             //talon.enableBrakeMode(true);    
-//         }
-//         // System.out.println(percent);
+        kPreviosPosition = talon.getSelectedSensorPosition();
+        
+    }
 
-//         talon.set(ControlMode.PercentOutput, percent); 
-//         // set talon speed based on input from XboxController.getleftY(), ie the input range on left y should map to the speed, where both the speed and the left joy stick is in range -1,1
-//     }
-// }
+    // Singleton class, call getInstance to access instead of the constructor.
+    public static Arm getInstance() {
+        if (instance == null) {
+            instance = new Arm();
+        }
+        return instance;
+    }
+
+    public void setPercentVoltage(double percentSpeed) {
+        
+        double distance = talon.getSelectedSensorPosition() - kPreviosPosition;
+        kAngle += distance * kAngleIncrement;
+        kPreviosPosition = talon.getSelectedSensorPosition();
+        
+        // second arg is in rad/sec
+        double feedOut = m_feedforward.calculate(kAngle * (Math.PI / 180), percentSpeed * kVelocityMultiplier);
+
+        talon.setVoltage(feedOut);
+    }
+    
+}
